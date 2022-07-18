37e4bca7e143dcdb97f80708035d1e2975ecfc92
Marko Radojević <marko.radojevic@s4s.rs>
2022-07-18 12:03:25 +0200

    Adding item(s) to the cart (local storage)

---
+++ eShop.CoreBusiness/Services/OrderService.cs
@@ -0,0 +1,67 @@
+﻿using eShop.CoreBusiness.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace eShop.CoreBusiness.Services
+{
+    public class OrderService : IOrderService
+    {
+        public bool ValidateCustomerInformation(string name, string address, string city, string province, string country)
+        {
+            if (string.IsNullOrWhiteSpace(name) || string.IsNullOrWhiteSpace(address) || string.IsNullOrWhiteSpace(city)
+                || string.IsNullOrWhiteSpace(province) || string.IsNullOrWhiteSpace(country))
+                return false;
+
+            return true;
+        }
+
+        public bool ValidateCreateOrder(Order order)
+        {
+            if (order == null) return false;
+
+            if (order.LineItems == null || order.LineItems.Count <= 0) return false;
+
+            foreach (var item in order.LineItems)
+            {
+                if (item.ProductId <= 0 || item.Price < 0 || item.Quantity <= 0) return false;
+            }
+
+            if (!this.ValidateCustomerInformation(order.CustomerName, order.CustomerAddress, order.CustomerCity,
+                                                  order.CustomerStateProvince, order.CustomerCountry))
+                return false;
+
+            return true;
+        }
+
+        public bool ValidateUpdateOrder(Order order)
+        {
+            if (order == null) return false;
+            if (!order.OrderId.HasValue) return false;
+
+            if (order.LineItems == null || order.LineItems.Count <= 0) return false;
+
+            if (!order.DatePlaced.HasValue) return false;
+
+            if (order.DateProcessed.HasValue || order.DateProcessing.HasValue) return false;
+
+            foreach (var item in order.LineItems)
+            {
+                if (item.ProductId <= 0 || item.Price < 0 || item.Quantity <= 0) return false;
+            }
+
+            if (!this.ValidateCustomerInformation(order.CustomerName, order.CustomerAddress, order.CustomerCity,
+                                                  order.CustomerStateProvince, order.CustomerCountry))
+                return false;
+
+            return true;
+        }
+
+        public bool ValidateProcessOrder(Order order)
+        {
+            if (!order.DateProcessed.HasValue || string.IsNullOrWhiteSpace(order.AdminUser)) 
+                return false;
+            return true;
+        }
+    }
+}
