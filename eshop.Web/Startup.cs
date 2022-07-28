f8ec7556be7c84435b507c5847d97f6b8586a20b
Marko Radojević <marko.radojevic@s4s.rs>
2022-07-28 15:44:00 +0200

    Authentication and Authorization

--- eshop.Web/Startup.cs
+++ eshop.Web/Startup.cs
@@ -40,6 +40,14 @@ namespace eshop.Web
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddControllers();
+            services.AddAuthentication("eShop.CookieAuth")
+                .AddCookie("eShop.CookieAuth", config =>
+                {
+                    config.Cookie.Name = "eShop.CookieAuth";
+                    config.LoginPath = "/authenticate";
+                });
+
             services.AddRazorPages();
             services.AddServerSideBlazor();
             services.AddSingleton<WeatherForecastService>();
@@ -85,8 +93,12 @@ namespace eshop.Web
 
             app.UseRouting();
 
+            app.UseAuthentication();
+            app.UseAuthorization();
+
             app.UseEndpoints(endpoints =>
             {
+                endpoints.MapControllers();
                 endpoints.MapBlazorHub();
                 endpoints.MapFallbackToPage("/_Host");
             });
